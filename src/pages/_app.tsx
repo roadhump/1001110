beca17b57ffaea1e9945ebacd8a872482d4e00bb
Aliaksei Shytkin <e79eas@gmail.com>
2021-12-12 13:52:19 +0300

    Add simple auth (refs #2)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,60 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Amplify, Auth, Hub } from 'aws-amplify';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './../aws-exports';
+import type {AppProps} from 'next/app';
+import { useEffect, useState } from 'react';
+import { CognitoUserAmplify } from '@aws-amplify/ui';
+Amplify.configure(awsExports);
+
+const MyApp = ({Component, pageProps}: AppProps) => {
+
+  const [user, setUser] = useState<CognitoUserAmplify>()
+
+  useEffect(() => {
+    Hub.listen('auth', async ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          setUser(await Auth.currentAuthenticatedUser());
+          break;
+        case 'signOut':
+          setUser(void 0);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+
+  }, []);
+
+  useEffect(() => {
+    const run = async () => {
+      setUser(await Auth.currentAuthenticatedUser());
+    }
+
+    run();
+
+  }, [])
+
+  return (
+    <Authenticator socialProviders={['facebook', 'google', 'apple',]}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {(user as any).attributes.name}</h1>
+          <button onClick={signOut}>Sign out</button>
+
+          <Component {...pageProps} />
+
+        </main>
+      )}
+    </Authenticator>
+  );
 }
+
 export default MyApp
